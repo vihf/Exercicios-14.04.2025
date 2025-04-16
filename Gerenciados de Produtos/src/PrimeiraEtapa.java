d70c2f1db1c0f82f88b8756ad0dd30f380bb339d
VINICIUS HENRIQUE FERREIRA <vinicius.ferreira6@alunos.sc.senac.br>
2025-04-16 19:33:26 -0300

    Primeira etapa

---
+++ Gerenciados de Produtos/src/PrimeiraEtapa.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class PrimeiraEtapa {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Escolha uma opção: ");
+            System.out.println("\n1. Cadastrar produto");
+            System.out.println("2. Listar produto");
+            System.out.println("3. Excluir produto");
+            System.out.println("4. Alterar produto");
+            System.out.println("5. Sair");
+            int opcao = scanner.nextInt();
+            scanner.nextLine();
+        }
+    }
+}
